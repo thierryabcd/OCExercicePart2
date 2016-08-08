002498a0b277a2a17af175ef22c77a11e3c83a89
thierry.basset <bassetthierry@yahoo.fr>
2016-08-08 11:33:51 +0200

    Correct spelling error : matrce , on line 8

--- cpp11_test.cpp
+++ cpp11_test.cpp
@@ -5,7 +5,7 @@ int main()
 {
      std::vector<std::vector<double>> matrice { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
 
-     std::cout << "Bonjour, voici une matrce:" << std::endl;
+     std::cout << "Bonjour, voici une matrice:" << std::endl;
      for (auto element : matrice) {
 	  for (auto val : element) {
 	       std::cout << val << " ";
